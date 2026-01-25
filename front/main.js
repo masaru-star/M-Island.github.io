a59b16725e372e18d97336933b86cccdb268659c
Ohka_M. <masaruk967@gmail.com>
2026-01-25 11:16:59 +0900

    Update main.js

--- front/main.js
+++ front/main.js
@@ -35,15 +35,6 @@ const MONSTER_TYPES = {
       maxFuel: 1000,
       maxAmmo: 1200
   };
-function factorial(n) {
-    if (n < 0) return NaN;
-    if (n === 0 || n === 1) return 1;
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
 /**
  * 軍艦がダメージを受けた際に、火災または弾薬庫の発火を判定する
  * @param {object} warship ダメージを受けた軍艦オブジェクト
